6e313e90bcf67849323a9190d84276730d66389d
Phillip Malboeuf <phil@phils.computer>
2023-10-11 14:39:46 -0400

    fr link fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,7 +66,7 @@
 			{#if $locale === 'fr-CA'}
 			<a href="https://arketypeworld.com">English</a>
 			{:else}
-			<a href="https:/fr.arketypeworld.com">Français</a>
+			<a href="https://fr.arketypeworld.com">Français</a>
 			{/if}
 		</nav>
 	</footer>
