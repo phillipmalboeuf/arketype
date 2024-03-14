975bfe90b9184c0edbfab607fcc32737936a3ac4
Phillip Malboeuf <phil@phils.computer>
2024-03-14 10:05:17 -0400

    player format fix

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -35,6 +35,7 @@
 	})
 
 	function format(url: string) {
+		if (url.includes('player.vimeo.com')) return url;
 		const split = url.replace('?share=copy', '').replace('https://vimeo.com/arketype/review', 'https://player.vimeo.com/video').replace('https://vimeo.com', 'https://player.vimeo.com/video').split('/')
 		return split.filter((s, i) => i < 5).join('/') + `?h=${split[5]}`
 	}
